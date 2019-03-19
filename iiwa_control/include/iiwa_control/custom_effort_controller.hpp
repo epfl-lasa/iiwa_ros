b0bc7cedbb14a7516721686fc10f7fbebf56d601
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2019-03-19 11:20:59 +0100

    Formatting and cleaning

--- iiwa_control/include/iiwa_control/custom_effort_controller.hpp
+++ iiwa_control/include/iiwa_control/custom_effort_controller.hpp
@@ -1,72 +1,71 @@
-#ifndef EFFORT_CONTROLLERS__EFFORT_CUSTOM_CONTROLLER_H
-#define EFFORT_CONTROLLERS__EFFORT_CUSTOM_CONTROLLER_H
+#ifndef IIWA_CONTROL_CUSTOM_EFFORT_CONTROLLER_H
+#define IIWA_CONTROL_CUSTOM_EFFORT_CONTROLLER_H
 
+// ROS headers
 #include <ros/node_handle.h>
-#include <urdf/model.h>
-#include <control_toolbox/pid.h>
-#include <boost/scoped_ptr.hpp>
-#include <realtime_tools/realtime_publisher.h>
-#include <hardware_interface/joint_command_interface.h>
+// ros control
 #include <controller_interface/controller.h>
-#include <std_msgs/Float64MultiArray.h>
-#include <control_msgs/JointControllerState.h>
+#include <hardware_interface/joint_command_interface.h>
+
+// realtime tools
 #include <realtime_tools/realtime_buffer.h>
+#include <realtime_tools/realtime_publisher.h>
 
-#include <control_stack/controllers/passive_ds.hpp>
+// msgs
+#include <std_msgs/Float64MultiArray.h>
 
+// URDF
+#include <urdf/model.h>
+
+// iiwa_tools
 // #include <iiwa_tools/GetGravity.h>
 #include <iiwa_tools/GetJacobian.h>
 
-namespace iiwa_control
-{
-    class CustomEffortController : public  controller_interface::Controller<hardware_interface::EffortJointInterface>
-    {
+// Control stack headers
+#include <control_stack/controllers/passive_ds.hpp>
+
+namespace iiwa_control {
+    class CustomEffortController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
     public:
         CustomEffortController();
         ~CustomEffortController();
 
-        bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n);
+        bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle& n);
 
         void update(const ros::Time& /*time*/, const ros::Duration& /*period*/);
 
         std::vector<hardware_interface::JointHandle> joints_;
 
-        realtime_tools::RealtimeBuffer<std::vector<double> > commands_buffer_;
+        realtime_tools::RealtimeBuffer<std::vector<double>> commands_buffer_;
 
         unsigned int n_joints_;
 
         std::vector<std::string> joint_names_;
 
-    private:
+    protected:
         ros::Subscriber sub_command_;
 
         // Controller
         control_stack::controllers::PassiveDS passive_ds_;
-        
-        // Controller's settings
-        unsigned int loop_count_,
-                     space_dim_;
 
+        // Controller's settings
+        unsigned int space_dim_;
         std::string operation_space_, gravity_comp_;
 
-        // Controller's service
-        ros::ServiceClient iiwa_client_gravity_,
-                           iiwa_client_jacobian_;
-        
+        // iiwa_tools services
+        ros::ServiceClient iiwa_client_gravity_, iiwa_client_jacobian_;
         // iiwa_tools::GetGravity gravity_srv_;
-
         iiwa_tools::GetJacobian jacobian_srv_;
 
-        // Others
+        // URDF
         std::vector<urdf::JointConstSharedPtr> joint_urdfs_;
 
+        // Command callback
         void commandCB(const std_msgs::Float64MultiArrayConstPtr& msg);
 
-        // Enforce limits.. seems to make sense only if command is position (check it)
-        void enforceJointLimits(double &command, unsigned int index);
+        // Enforce effort limits
+        void enforceJointLimits(double& command, unsigned int index);
+    };
+} // namespace iiwa_control
 
-    };  
-
-} // namespace
-
-#endif // EFFORT_CONTROLLERS__EFFORT_CUSTOM_CONTROLLER_H
+#endif
--- iiwa_control/src/custom_effort_controller.cpp
+++ iiwa_control/src/custom_effort_controller.cpp
@@ -1,12 +1,10 @@
-#include <pluginlib/class_list_macros.hpp>
-#include <iiwa_control/custom_effort_controller.hpp>
-
 #include <Eigen/Core>
 
-#include <iiwa_tools/iiwa_tools.h>
+#include <pluginlib/class_list_macros.hpp>
 
-namespace iiwa_control
-{
+#include <iiwa_control/custom_effort_controller.hpp>
+
+namespace iiwa_control {
     double get_multi_array(const std_msgs::Float64MultiArray& array, size_t i, size_t j)
     {
         assert(array.layout.dim.size() == 2);
@@ -19,26 +17,24 @@ namespace iiwa_control
 
     CustomEffortController::~CustomEffortController() { sub_command_.shutdown(); }
 
-    bool CustomEffortController::init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n)
+    bool CustomEffortController::init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle& n)
     {
         // List of controlled joints
         std::string param_name = "joints";
-        if(!n.getParam(param_name, joint_names_))
-        {
+        if (!n.getParam(param_name, joint_names_)) {
             ROS_ERROR_STREAM("Failed to getParam '" << param_name << "' (namespace: " << n.getNamespace() << ").");
             return false;
         }
         n_joints_ = joint_names_.size();
 
-        if(n_joints_ == 0){
+        if (n_joints_ == 0) {
             ROS_ERROR_STREAM("List of joint names is empty.");
             return false;
         }
 
         // Get URDF
         urdf::Model urdf;
-        if (!urdf.initParam("robot_description"))
-        {
+        if (!urdf.initParam("robot_description")) {
             ROS_ERROR("Failed to parse urdf file");
             return false;
         }
@@ -51,7 +47,7 @@ namespace iiwa_control
         n.getParam("params/eigvals", eigvals_vec);
 
         // Check the operational space
-        if (operation_space_ == "task"){
+        if (operation_space_ == "task") {
             space_dim_ = 6;
             iiwa_client_jacobian_ = n.serviceClient<iiwa_tools::GetJacobian>("/iiwa/iiwa_jacobian_server");
         }
@@ -61,21 +57,17 @@ namespace iiwa_control
         // Init Controller
         passive_ds_.SetParams(space_dim_, eigvals_vec);
 
-        for(unsigned int i=0; i<n_joints_; i++)
-        {
-            try
-            {
+        for (unsigned int i = 0; i < n_joints_; i++) {
+            try {
                 joints_.push_back(hw->getHandle(joint_names_[i]));
             }
-            catch (const hardware_interface::HardwareInterfaceException& e)
-            {
+            catch (const hardware_interface::HardwareInterfaceException& e) {
                 ROS_ERROR_STREAM("Exception thrown: " << e.what());
                 return false;
             }
 
             urdf::JointConstSharedPtr joint_urdf = urdf.getJoint(joint_names_[i]);
-            if (!joint_urdf)
-            {
+            if (!joint_urdf) {
                 ROS_ERROR("Could not find joint '%s' in urdf", joint_names_[i].c_str());
                 return false;
             }
@@ -95,8 +87,8 @@ namespace iiwa_control
 
     void CustomEffortController::update(const ros::Time& time, const ros::Duration& period)
     {
-        std::vector<double> & commands = *commands_buffer_.readFromRT();
-        
+        std::vector<double>& commands = *commands_buffer_.readFromRT();
+
         Eigen::MatrixXd jac(6, n_joints_);
         bool jac_valid = false;
 
@@ -112,7 +104,7 @@ namespace iiwa_control
                 assert(jacobian_srv_.response.jacobian.layout.dim[0].size == 6); // check if Jacobian has proper dimensions
                 assert(jacobian_srv_.response.jacobian.layout.dim[1].size == n_joints_);
 
-                for(size_t r = 0; r < 6; r++) {
+                for (size_t r = 0; r < 6; r++) {
                     for (size_t c = 0; c < n_joints_; c++) {
                         jac(r, c) = get_multi_array(jacobian_srv_.response.jacobian, r, c);
                     }
@@ -128,8 +120,7 @@ namespace iiwa_control
         Eigen::VectorXd desired_vel(n_joints_), curr_vel(n_joints_);
 
         desired_vel = Eigen::VectorXd::Map(commands.data(), commands.size());
-        for(unsigned int i=0; i<n_joints_; i++)
-        {
+        for (unsigned int i = 0; i < n_joints_; i++) {
             curr_vel(i) = joints_[i].getVelocity();
         }
 
@@ -153,7 +144,7 @@ namespace iiwa_control
 
         Eigen::VectorXd::Map(commanded_effort.data(), commanded_effort.size()) = output;
 
-        for(unsigned int i=0; i<n_joints_; i++) {
+        for (unsigned int i = 0; i < n_joints_; i++) {
             enforceJointLimits(commanded_effort[i], i);
             joints_[i].setCommand(commanded_effort[i]);
         }
@@ -161,8 +152,7 @@ namespace iiwa_control
 
     void CustomEffortController::commandCB(const std_msgs::Float64MultiArrayConstPtr& msg)
     {
-        if(msg->data.size() != n_joints_ && msg->data.size() != 6)
-        {
+        if (msg->data.size() != n_joints_ && msg->data.size() != 6) {
             ROS_ERROR_STREAM("Dimension of command (" << msg->data.size() << ") is not correct! Not executing!");
             return;
         }
@@ -170,18 +160,18 @@ namespace iiwa_control
         commands_buffer_.writeFromNonRT(msg->data);
     }
 
-    void CustomEffortController::enforceJointLimits(double &command, unsigned int index)
+    void CustomEffortController::enforceJointLimits(double& command, unsigned int index)
     {
         // Check that this joint has applicable limits
-        if(command > joint_urdfs_[index]->limits->effort) // above upper limit
+        if (command > joint_urdfs_[index]->limits->effort) // above upper limit
         {
             command = joint_urdfs_[index]->limits->effort;
         }
-        else if(command < -joint_urdfs_[index]->limits->effort) // below lower limit
+        else if (command < -joint_urdfs_[index]->limits->effort) // below lower limit
         {
             command = -joint_urdfs_[index]->limits->effort;
         }
     }
-} // namespace
+} // namespace iiwa_control
 
 PLUGINLIB_EXPORT_CLASS(iiwa_control::CustomEffortController, controller_interface::ControllerBase)
