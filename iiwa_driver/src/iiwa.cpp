a62e0e0f9b981e2bd1d9c461fa974a2e05b929d1
Matthias Mayr <matthias.mayr@cs.lth.se>
2022-11-15 22:25:39 +0100

    Chore: std::sin, size_t in loops and assignment
    Fixes a couple of minor things.

--- iiwa_driver/src/iiwa.cpp
+++ iiwa_driver/src/iiwa.cpp
@@ -33,7 +33,6 @@
 // FRI Headers
 #include <kuka/fri/ClientData.h>
 
-#include <cmath>
 #include <thread>
 
 namespace iiwa_ros {
@@ -116,7 +115,7 @@ namespace iiwa_ros {
             ROS_WARN_STREAM_NAMED("Iiwa", "Could not read URDF from '" << _robot_description << "' parameters. Joint limits will not work.");
 
         // Initialize Controller
-        for (int i = 0; i < _num_joints; ++i) {
+        for (size_t i = 0; i < _num_joints; ++i) {
             _joint_position[i] = _joint_velocity[i] = _joint_effort[i] = 0.;
             // Create joint state interface
             hardware_interface::JointStateHandle joint_state_handle(_joint_names[i], &_joint_position[i], &_joint_velocity[i], &_joint_effort[i]);
@@ -281,7 +280,7 @@ namespace iiwa_ros {
         // Update ROS structures
         _joint_position_prev = _joint_position;
 
-        for (int i = 0; i < _num_joints; i++) {
+        for (size_t i = 0; i < _num_joints; i++) {
             _joint_position[i] = _robot_state.getMeasuredJointPosition()[i];
             _joint_velocity[i] = filters::exponentialSmoothing((_joint_position[i] - _joint_position_prev[i]) / elapsed_time.toSec(), _joint_velocity[i], 0.2);
             _joint_effort[i] = _robot_state.getMeasuredTorque()[i];
@@ -305,13 +304,12 @@ namespace iiwa_ros {
         _fri_message_data->resetCommandMessage();
 
         if (_robot_state.getClientCommandMode() == kuka::fri::TORQUE) {
-            _joint_position_command = _joint_position;
             // Implements dithering to trigger the friction observer.
             // If the physical robot is at the commanded positions, KUKA turns off friction
             // compensation.
-            for (int i = 0; i < 7; i++)
+            for (size_t i = 0; i < 7; i++)
             {
-                _joint_position_command.at(i) += 0.1 * sin(ros::Time::now().toSec());
+                _joint_position_command.at(i) = _joint_position.at(i) + 0.1 * std::sin(ros::Time::now().toSec());
             }
             _robot_command.setJointPosition(_joint_position_command.data());
             _robot_command.setTorque(_joint_effort_command.data());
