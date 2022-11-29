128ed56d4af3b2e16e3e36500ed1f5608223b805
Matthias Mayr <matthias.mayr@cs.lth.se>
2022-11-29 13:55:37 +0100

    New: Basis for JOSS paper

---
+++ iiwa_ros/paper/compile_paper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "paper" ]
+then
+    echo -e "This script should be run in the iiwa_ros package folder ('roscd iiwa_ros'). Exiting."
+    exit -1
+fi
+
+docker run --rm \
+    --volume $PWD/paper:/data \
+    --user $(id -u):$(id -g) \
+    --env JOURNAL=joss \
+    openjournals/inara
