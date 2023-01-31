7cf01840172fc6981c6bfe41b288345317a15654
niederha <loic.niederhauser@epfl.ch>
2023-01-31 13:19:31 +0100

    Added notes on what to do in the script

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# If not installed, install docker with post install
+
+# If nvidia, install nividia-docker2
+
 # Install aica docker
-if ! command -v aica-docker &> /dev/null
-then
-    echo "Installing AICA docker"
+if [ ! command -v aica-docker &> /dev/null ]; then
+    echo "Installing aica-docker..."
     git clone git@github.com:aica-technology/docker-images.git /tmp
-    sudo bash /tmp/docker-images/scripts/install-aica-docker-sh
+    sudo bash /tmp/docker-images/scripts/install-aica-docker.sh
     rm -rf /tmp/docker-images
+    echo "aica-docker properly installed"
 else
     echo "AICA docker already found"
 fi
