628a28125ef89dc5c39d2ea302a808cb7fe6f5f8
niederha <loic.niederhauser@epfl.ch>
2023-01-31 13:19:05 +0100

    Corrected default use_smid flag to OFF

--- docker/build_docker.bash
+++ docker/build_docker.bash
@@ -3,7 +3,7 @@
 # Name and base and options
 IMAGE_NAME=epfl-lasa/iiwa_ros                             # Chose any name for your image (but make sure to report it in start_docker)
 ROS_DISTRO=noetic                                         # Possible: noetic, melodic
-USE_SIMD=ON                                               # Possible: ON, OFF
+USE_SIMD=OFF                                              # Possible: ON, OFF
 BASE_IMAGE=ghcr.io/aica-technology/ros-ws:${ROS_DISTRO}   # Do not modify
 
 # Help
