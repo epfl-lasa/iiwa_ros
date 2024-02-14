4967b46320b5d77890fea391d9a40d7a9e578c5e
S3C Service Account <s3c.github@gmail.com>
2024-02-14 11:27:16 +0100

    Pseudo inverse of jacobian with orthogonal decomposition
    - Better numerical stability (refer to Eigen documentation)
    - More efficient

--- iiwa_control/src/custom_effort_controller.cpp
+++ iiwa_control/src/custom_effort_controller.cpp
@@ -45,24 +45,8 @@ pseudo_inverse(const MatT& mat,
                typename MatT::Scalar tolerance = typename MatT::Scalar{
                    1e-4})  // choose appropriately
 {
-    typedef typename MatT::Scalar Scalar;
-    auto svd = mat.jacobiSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
-    const auto& singularValues = svd.singularValues();
-    Eigen::Matrix<Scalar, MatT::ColsAtCompileTime, MatT::RowsAtCompileTime>
-        singularValuesInv(mat.cols(), mat.rows());
-    singularValuesInv.setZero();
-    for (unsigned int i = 0; i < singularValues.size(); ++i)
-    {
-        if (singularValues(i) > tolerance)
-        {
-            singularValuesInv(i, i) = Scalar{1} / singularValues(i);
-        }
-        else
-        {
-            singularValuesInv(i, i) = Scalar{0};
-        }
-    }
-    return svd.matrixV() * singularValuesInv * svd.matrixU().adjoint();
+    // More efficient (~2x) and numerically stable than with jacobiSvd
+    return mat.completeOrthogonalDecomposition().pseudoInverse();
 }
 
 std::vector<std::vector<std::string>> get_types(const std::string& input,
